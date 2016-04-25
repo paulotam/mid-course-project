cc9ae5bbb0c2bf8f7c8a5ae3ecf295819ae087ae
Paulo Tam <Paulo.Tam@sportsbet.com.au>
2016-04-25 22:24:10 +1000

    Buttons 0 - 3, plus and minus.

---
+++ mid-course-project/mid-course-project/Calculator.swift
@@ -0,0 +1,102 @@
+//
+//  Calculator.swift
+//  mid-course-project
+//
+//  Created by Paulo Tam on 25/04/2016.
+//  Copyright © 2016 Paulo Tam. All rights reserved.
+//
+
+class Calculator {
+  
+  var currentString: String
+  var currentNumber: Double?
+  var currentNumber2: Double?
+  
+  var currentOperation = ""
+  
+  var currentTotal:Double = 0
+  
+  init() {
+    currentString = ""
+    currentNumber = 0
+    currentOperation = ""
+    currentNumber2 = 0
+    clear()
+  }
+  
+  func resetInternals() {
+    currentString = ""
+    currentOperation = ""
+    currentNumber = 0
+    currentNumber2 = 0
+  }
+  
+  
+  func plus() -> Double {
+    let value: Double = 0
+    return value
+    
+  }
+  
+  func getCurrentValue() -> Double {
+    return currentTotal
+  }
+  
+  func addNumber(value: Double!) -> String {
+    if currentOperation == "" {
+      currentNumber = setValue(currentNumber!, additionalDigit: value!)
+      currentString = forTailingZero(currentNumber!)
+    } else {
+      currentNumber2 = setValue(currentNumber2!, additionalDigit: value!)
+      currentString = forTailingZero(currentNumber2!)
+    }
+    return currentString
+  }
+  
+  func setValue(currentDigit: Double, additionalDigit: Double) -> Double {
+    var returnDigit: Double = 0
+    if currentDigit == 0 {
+      returnDigit = additionalDigit
+    } else if currentNumber > 0 {
+      returnDigit = (currentDigit * 10 ) + additionalDigit
+    }
+    return returnDigit
+  }
+  
+  func setOperation(operation: String) {
+    if currentOperation == "" {
+      currentOperation = operation
+    } else {
+      //do action then
+      currentNumber2 = 0
+      currentOperation = operation
+    }
+  }
+  
+  func process() -> String {
+    switch currentOperation {
+    case "+":
+      currentTotal = currentNumber! + currentNumber2!
+      currentNumber = currentTotal
+    case "-":
+      currentTotal = currentNumber! - currentNumber2!
+      currentNumber = currentTotal
+    default:
+        currentTotal = currentNumber!
+    }
+    currentString = forTailingZero(currentTotal)
+    return currentString
+  }
+  
+  func clear() -> String {
+    resetInternals()
+    return forTailingZero(0)
+  }
+  
+  func forTailingZero(temp: Double) -> String{
+    let tempVar = String(format: "%g", temp)
+    return tempVar
+  }
+
+  
+}
--- mid-course-project/mid-course-project/ViewController.swift
+++ mid-course-project/mid-course-project/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var myCalc = Calculator()
+  @IBOutlet weak var displayField: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    display(myCalc.clear())
+    
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func button0(sender: AnyObject) {
+    display(myCalc.addNumber(Double(0)))
+  }
 
+  @IBAction func button1(sender: AnyObject) {
+    display(myCalc.addNumber(Double(1)))
+  }
 
+  @IBAction func button2(sender: AnyObject) {
+    display(myCalc.addNumber(Double(2)))
+  }
+  
+  @IBAction func button3(sender: AnyObject) {
+    display(myCalc.addNumber(Double(3)))
+  }
+  
+  @IBAction func buttonDot(sender: AnyObject) {
+  }
+  
+  @IBAction func buttonPlus(sender: AnyObject) {
+    myCalc.setOperation("+")
+  }
+  
+  @IBAction func buttonMinus(sender: AnyObject) {
+    myCalc.setOperation("-")
+  }
+  
+  
+  @IBAction func buttonEquals(sender: AnyObject) {
+    display(myCalc.process())
+  }
+  
+  func display(textValue: String) {
+    displayField.text = textValue
+  }
+  
+//  func display(doubleValue: Double) {
+//    displayField.text = String(doubleValue)
+//  }
+  
+  @IBAction func buttonClear(sender: AnyObject) {
+    display(myCalc.clear())
+  }
 }
 
